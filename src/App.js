a2045fbfb63f308732a721fab06e74a42bd34da5
unknown <galaxy.net0710@gmail.com>
2020-03-02 18:58:53 -0600

    fetch anidado pokeapi react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  
+  const [result, setResult] = React.useState([]);
+  const [poke, setPoke] = React.useState([]);
+  const [load, setLoad] = React.useState('true');
+
+  const arr = [];
+  
+  useEffect(() => {
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
+      .then((response) => response.json())
+      .then((data) => setResult(
+        data.results.map((item) => {
+          fetch(item.url)
+            .then((response) => response.json())
+            .then((allpokemon) => arr.push(allpokemon));
+            setPoke(arr);
+        }),
+      ));
+  }, []);
+
+   setTimeout(() => {
+     setLoad(false);
+     console.log(poke)
+   }, 1000);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='pokegallery'>
+
+        { load ? (
+          <p>Loading...</p>
+        ) : (
+
+          poke.map((img, i) => (
+            <div id={img.id} key={img.id}>
+
+              <div className='card' style={{ width: '10rem', height: '15rem', backgroundColor: '#F0F0C9' }}>
+                <img  src={img.sprites.front_default} alt='pokemon' />
+                <div >
+                  <h5 >{img.name}</h5>
+                  <h6>type: {img.types[0].type.name}</h6>
+                </div>
+              </div>
+
+
+            </div>
+          ))
+        )}
+
+
+
+</div>
     </div>
   );
 }
