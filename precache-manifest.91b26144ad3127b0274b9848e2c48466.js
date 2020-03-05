b30f0bef943bd9f12740b133a9b28302ed0b6852
NetoCruz <galaxy.net0710@gmail.com>
2020-03-05 14:19:55 -0600

    Updates

---
+++ precache-manifest.91b26144ad3127b0274b9848e2c48466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15049264b010db4cd1ac67abe89fe7d8",
+    "url": "/FetchNestedPoke/index.html"
+  },
+  {
+    "revision": "cc6ef7a3125dcb0c24e8",
+    "url": "/FetchNestedPoke/static/css/main.d8fb7a3e.chunk.css"
+  },
+  {
+    "revision": "636d95f8f2b51beee5ac",
+    "url": "/FetchNestedPoke/static/js/2.70789099.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FetchNestedPoke/static/js/2.70789099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6ef7a3125dcb0c24e8",
+    "url": "/FetchNestedPoke/static/js/main.84bfb8ce.chunk.js"
+  },
+  {
+    "revision": "cfcce2c6921805a69603",
+    "url": "/FetchNestedPoke/static/js/runtime-main.4b077c32.js"
+  }
+]);
